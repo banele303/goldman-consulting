97477b08e20c16490fcde03701a11f533b9ed2e5
blessed <sibandablessed724@gmail.com>
2024-12-15 12:24:27 +0000

    yes its me

--- app/gallary/page.jsx
+++ app/gallary/page.jsx
@@ -1,5 +1,5 @@
 
-import GalleryGrid from '../components/GalleryGrid'
+
 import { Navbar } from "../components/ui/Navbar";
 export default function GalleryPage() {
   return (
@@ -7,7 +7,7 @@ export default function GalleryPage() {
  <Navbar/>
       <main className="flex-grow container mx-auto px-4 py-8">
         <h1 className="text-4xl font-bold text-center mb-12">Our Work</h1>
-        <GalleryGrid />
+       
       </main>
      
     </div>
